19eb07a8affe697de3ffb1ded2cc8cbe464bd77f
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2021-03-17 14:23:08 +0100

    initial commit

---
+++ src/icalendar-store.ts
@@ -0,0 +1,28 @@
+import {BaseResourceStore, BasicRepresentation, Representation, ResourceIdentifier} from "@solid/community-server";
+import fetch from 'node-fetch';
+const ICAL = require('ical.js');
+
+export class ICalendarStore extends BaseResourceStore {
+    private readonly url: string;
+
+    public constructor(options: {
+        url: string;
+    }) {
+        super();
+        this.url = options.url;
+    }
+
+    /**
+     * Retrieves a JSON representation of events in the calender.
+     */
+    public async getRepresentation(identifier: ResourceIdentifier): Promise<Representation> {
+        const response = await fetch(this.url);
+        const text = await response.text();
+        const jcalData = ICAL.parse(text);
+        const vcalendar = new ICAL.Component(jcalData);
+        const vevent = vcalendar.getFirstSubcomponent('vevent');
+        const summary = vevent.getFirstPropertyValue('summary');
+
+        return new BasicRepresentation(summary, identifier);
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * from './icalendar-store';
