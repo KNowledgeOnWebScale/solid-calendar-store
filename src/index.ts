d100c18f162887ca836071495acd2845e63d4a88
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2021-04-08 14:58:22 +0200

    turn hide title of busy events

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './http-get-store';
 export * from './ics-to-json-converter';
 export * from './json-to-rdf-converter';
 export * from './availability-store';
+export * from './busy-store';
