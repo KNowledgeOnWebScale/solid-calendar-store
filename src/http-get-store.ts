23c9cd9ed3e171b00082640001d6b794cc1408b9
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2021-04-08 15:40:17 +0200

    create aggregate store

--- src/http-get-store.ts
+++ src/http-get-store.ts
@@ -1,5 +1,6 @@
 import {BaseResourceStore, BasicRepresentation, Representation, ResourceIdentifier} from "@solid/community-server";
 import fetch from 'node-fetch';
+import {RepresentationPreferences} from "@solid/community-server/dist/ldp/representation/RepresentationPreferences";
 const parseContentType = require('content-type').parse;
 
 export class HttpGetStore extends BaseResourceStore {
@@ -15,7 +16,7 @@ export class HttpGetStore extends BaseResourceStore {
     /**
      * Retrieves a JSON representation of events in the calender.
      */
-    public async getRepresentation(identifier: ResourceIdentifier): Promise<Representation> {
+    public async getRepresentation(identifier: ResourceIdentifier, preferences: RepresentationPreferences): Promise<Representation> {
         const response = await fetch(this.url);
         const text = await response.text();
         let contentType = response.headers.get('content-type') || 'text/plain';
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './ics-to-json-converter';
 export * from './json-to-rdf-converter';
 export * from './availability-store';
 export * from './busy-store';
+export * from './aggregate-store';
